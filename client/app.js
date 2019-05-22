cb67541045e2b0ec159a14d7e3c0ef192fb1931d
yongchengzhu <yongchengzhu002@gmail.com>
2019-05-22 17:25:04 -0400

    Connected express to mysql and display the current number of users in the database in root route.

--- client/app.js
+++ client/app.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const mysql   = require('mysql');
 const app     = express();
 
-app.get('/', (req, res) => {
-  res.send('Homepage');
+// Connect to local MySQL
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123',
+  database: 'waitlist_db'
 });
 
+connection.connect((error) => {
+  if (error) throw error;
+  console.log('Connected to waitlist_db...');
+})
+
+// Route Handlers
+app.get('/', (req, res) => {
+  const q = 'SELECT COUNT(*) AS total_users FROM users';
+
+  connection.query(q, (error, results) => {
+    if (error) res.send(error);
+
+    res.send(`We have ${results[0].total_users} users in our database!`)
+  });
+});
+
+// Listen to port
 app.listen(8080, () => {
   console.log('Listening to port 8080...');
 });
